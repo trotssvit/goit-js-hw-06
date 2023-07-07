f0007091225c7b1aebf176d91db30f5be81b9e59
Svitlana Trotsenko <trots.svit@gmail.com>
2023-07-07 23:50:23 +0100

    2task

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+    'Potatoes',
+    'Mushrooms',
+    'Garlic',
+    'Tomatoes',
+    'Herbs',
+    'Condiments',
 ];
+const list = document.querySelector('#ingredients')
+const li = ingredients.map(name => {
+    const li = document.createElement('li');
+    li.classList.add('item')
+    li.textContent = name;
+    return li
+});
+
+console.log(li);
+list.append(...li);
