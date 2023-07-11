4835bcdcd1aef82427499df0c74079c1db4f1e5f
Svitlana Trotsenko <trots.svit@gmail.com>
2023-07-11 23:33:44 +0100

    Update task-01.js

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,12 +1,25 @@
-const categoriesList = document.querySelector('#categories');
-const categories = categoriesList.querySelectorAll('li.item');
+// const categoriesList = document.querySelector('#categories');
+// const categories = categoriesList.querySelectorAll('li.item');
 
-console.log('Number of categories:', categories.length);
+// console.log('Number of categories:', categories.length);
 
-categories.forEach(category => {
-    const title = category.querySelector('h2').textContent;
-    const items = category.querySelectorAll('li');
+// categories.forEach(category => {
+//     const title = category.querySelector('h2').textContent;
+//     const items = category.querySelectorAll('li');
 
-    console.log('Category:', title);
-    console.log('Number of items:', items.length);
+//     console.log('Category:', title);
+//     console.log('Number of items:', items.length);
+// });
+const categoriesList = document.querySelector("#categories");
+const categories = categoriesList.children;
+
+console.log("Number of categories:", categories.length);
+
+
+Array.from(categories).forEach((category) => {
+    const categoryTitle = category.firstElementChild.textContent;
+    const categoryElements = category.lastElementChild.children;
+
+    console.log("Category:", categoryTitle);
+    console.log("Elements:", categoryElements.length);
 });
